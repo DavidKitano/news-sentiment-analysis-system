81a9788512b70f3657f73c52ddeda4002e118e95
Kitano <kitano@influx.io>
2024-03-26 15:46:31 +0800

    fix: 5秒后跳转

--- src/views/auth/403.vue
+++ src/views/auth/403.vue
@@ -1,6 +1,18 @@
 <template>
   <auth-error>
     <h1>403 Forbidden - 禁止访问</h1>
-    <p>您被禁止访问该页面，请检查登录状态及是否拥有权限</p>
+    <p>您被禁止访问该页面，请检查登录状态及是否拥有权限，5秒后跳转至登录页面...</p>
   </auth-error>
 </template>
+<script lang="ts" setup>
+import { useAuth } from '@/stores/auth/auth'
+
+const auth = useAuth()
+
+onMounted(() => {
+  auth.clearAuth()
+  setTimeout(() => {
+    window.location.href = '/auth/login'
+  }, 5000)
+})
+</script>
